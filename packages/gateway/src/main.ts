c163f94f354833d124ba4b6b1226fe8f32a6c211
Dr. Georg Hackenberg <ghackenberg@gmail.com>
2025-05-29 08:52:32 +0200

    update nest, aedes, and typeorm versions

--- packages/gateway/src/main.ts
+++ packages/gateway/src/main.ts
@@ -4,22 +4,22 @@ import { createProxyMiddleware } from 'http-proxy-middleware'
 const app = express()
 
 app.use('/rest', createProxyMiddleware({
-    target: 'http://localhost:3001'
+    target: 'http://localhost:3001/rest'
 }))
 app.use('/rest-doc', createProxyMiddleware({
-    target: 'http://localhost:3001'
+    target: 'http://localhost:3001/rest-doc'
 }))
 app.use('/mqtt', createProxyMiddleware({
     target: 'http://localhost:3004/', ws: true
 }))
 app.use('/modules/worker', createProxyMiddleware({
-    target: 'http://localhost:3005'
+    target: 'http://localhost:3005/modules/worker'
 }))
 app.use('/scripts/worker', createProxyMiddleware({
-    target: 'http://localhost:3005'
+    target: 'http://localhost:3005/scripts/worker'
 }))
 app.use('/', createProxyMiddleware({
-    target: 'http://localhost:3002'
+    target: 'http://localhost:3002/'
 }))
 
 app.listen(3000)
